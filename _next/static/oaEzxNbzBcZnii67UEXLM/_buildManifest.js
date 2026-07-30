c601299d3037a74101e128177d40d9b45758007d
akankshadeulkar1-hubr <akankshadeular1@email.comm>
2026-07-30 22:45:36 +0530

    Updates

---
+++ _next/static/oaEzxNbzBcZnii67UEXLM/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/National_Triage_Command_AI//_next/:path+",
+        "destination": "/National_Triage_Command_AI/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
